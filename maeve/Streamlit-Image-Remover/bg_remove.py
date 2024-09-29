e2b9df3b709c5301353223a6a9b64b60e83dbc9e
Juna <115675811+junakim0118@users.noreply.github.com>
2024-09-29 01:48:53 -0400

    streamlit1

--- maeve/Streamlit-Image-Remover/bg_remove.py
+++ maeve/Streamlit-Image-Remover/bg_remove.py
@@ -6,10 +6,8 @@ from rembg import remove
 
 st.set_page_config(layout="wide", page_title="Image Background Remover")
 
-st.write("## Remove background from your image")
-st.write(
-    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](<https://github.com/tyler-simons/BackgroundRemoval>) on GitHub. Special thanks to the [rembg library](<https://github.com/danielgatis/rembg>) :grin:"
-)
+st.header("Remove background from your clothes!")
+
 st.sidebar.write("## Upload and download :gear:")
 
 # Create the columns
