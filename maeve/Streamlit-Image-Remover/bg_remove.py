bde8b0f06373eebcc87ae254dde960bb7ecf6e9f
Juna <115675811+junakim0118@users.noreply.github.com>
2024-09-29 01:28:19 -0400

    please work

--- maeve/Streamlit-Image-Remover/bg_remove.py
+++ maeve/Streamlit-Image-Remover/bg_remove.py
@@ -1,30 +1,46 @@
 from io import BytesIO
-from os import remove
 
 import streamlit as st
 from PIL import Image
+from rembg import remove
 
-st.header("Get your clothes with transparent background!")
+st.set_page_config(layout="wide", page_title="Image Background Remover")
 
-# Upload the file
-imageUploaded = st.file_uploader("Upload your clothes image", type=["png", "jpg", "jpeg"])
+st.write("## Remove background from your image")
+st.write(
+    ":dog: Try uploading an image to watch the background magically removed. Full quality images can be downloaded from the sidebar. This code is open source and available [here](<https://github.com/tyler-simons/BackgroundRemoval>) on GitHub. Special thanks to the [rembg library](<https://github.com/danielgatis/rembg>) :grin:"
+)
+st.sidebar.write("## Upload and download :gear:")
 
-# Convert the image to BytesIO so we can download it!
-def deleteBg(img):
-    buffer = BytesIO()
-    img.save(buffer, format="PNG")
-    byteIm = buffer.getvalue()
-    return byteIm
+# Create the columns
+col1, col2 = st.columns(2)
 
-# If we've uploaded an image, open it and remove the background!
-if imageUploaded:
-    # SHOW the uploaded image!
-    st.image(imageUploaded)
-    image = Image.open(imageUploaded)
+# Download the fixed image
+def convert_image(img):
+    buf = BytesIO()
+    img.save(buf, format="PNG")
+    byte_im = buf.getvalue()
+    return byte_im
+
+# Package the transform into a function
+def fix_image(upload):
+    image = Image.open(upload)
+    col1.write("Original Image :camera:")
+    col1.image(image)
+
     fixed = remove(image)
-    newImage = deleteBg(fixed)
-    # SHOW the improved image!
-    st.image(newImage)
-    st.downloadButton(
-        "Here is your clothes", newImage, "fixed.png", "image/png"
+    col2.write("Fixed Image :wrench:")
+    col2.image(fixed)
+    st.sidebar.markdown("\\n")
+    st.sidebar.download_button(
+        "Download fixed image", convert_image(fixed), "fixed.png", "image/png"
     )
+
+# Create the file uploader
+my_upload = st.sidebar.file_uploader("Upload an image", type=["png", "jpg", "jpeg"])
+
+# Fix the image!
+if my_upload is not None:
+    fix_image(upload=my_upload)
+else:
+    fix_image("./wallaby.png")
