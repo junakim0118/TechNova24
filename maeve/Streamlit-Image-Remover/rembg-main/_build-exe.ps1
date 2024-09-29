1982e76035b2c84dcccf249ac6776de91082ffce
Juna <115675811+junakim0118@users.noreply.github.com>
2024-09-29 02:09:32 -0400

    undo

--- maeve/Streamlit-Image-Remover/rembg-main/_build-exe.ps1
+++
@@ -1,9 +0,0 @@
-# Install required packages
-pip install pyinstaller
-pip install -e ".[cli]"
-
-# Create PyInstaller spec file with specified data collections
-# pyi-makespec --collect-data=gradio_client --collect-data=gradio rembg.py
-
-# Run PyInstaller with the generated spec file
-pyinstaller rembg.spec
